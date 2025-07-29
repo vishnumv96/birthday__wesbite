8895124d94f5a838c517e8efc38aae39077aa325
vishnumv96 <vishnumvv007@gmail.com>
2025-07-29 23:02:23 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    image_folder = os.path.join('static', 'images')
+    image_files = [f for f in os.listdir(image_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+
+    captions = [
+        "Best moments with you 🥰",
+        "Our craziness together 🤪",
+        "Always smiling with you 😄",
+        "Unforgettable memories 💖",
+        "To many more adventures 🚀"
+    ]
+
+    images = [{'file': f, 'caption': captions[i % len(captions)]} for i, f in enumerate(image_files)]
+    return render_template('index.html', images=images)
+
+if __name__ == '__main__':
+    app.run(debug=True)
